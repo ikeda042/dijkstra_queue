d2820327f74b4deabbc8518b4320577e695649ca
ikeda042 <ikeda042main@gmail.com>
2024-01-04 23:20:49 +0900

    バリアフリー経路探索

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from graphviz import Graph
 from components import PathFinder, AdjNode, transpose, add_matrices, generate_random_G
-
+from typing import cast
 #隣接行列の生成
 G = generate_random_G(15)
 
@@ -17,14 +17,16 @@ nodes, path_weights = [str(chr(i).upper()) for i in range(97,len(G)+97)], {}
 #GraphvizObjectにノード、エッジを追加
 for i in range(len(G)):
     for j in range(len(G[0])):
-        if G[i][j].weight != 0:
+        #エッジの重みが0の時はエッジがないとみなす
+        if cast(AdjNode,G[i][j]).weight != 0:
             path_weights[f"{nodes[i]}->{nodes[j]}"] = G[i][j]
+            #無向グラフなので、i>jのときはエッジを追加しない
             if i>j:
-                print(type(G[i][j]))
-                GraphvizObject.edge(str(nodes[i]),str(nodes[j]),label=str(G[i][j].weight),color='black' if G[i][j].is_Barrier_free else 'red')
+                #非バリアフリー経路を赤色で描画、それ以外を黒色で描画
+                GraphvizObject.edge(str(nodes[i]),str(nodes[j]),label=str(cast(AdjNode,G[i][j]).weight),color='black' if cast(AdjNode,G[i][j]).is_Barrier_free else 'red')
 GraphvizObject.render("G")
 
-path_weights = {i: path_weights[i].weight for i in path_weights.keys() if path_weights[i].is_Barrier_free}
+path_weights : dict[str,int] = {i: cast(AdjNode,path_weights[i]).weight for i in path_weights.keys() if cast(AdjNode,path_weights[i]).is_Barrier_free}
 print(path_weights)
 W = PathFinder(path_weights,nodes)
 
